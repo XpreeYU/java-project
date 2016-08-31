17a408239b6bec13cc308aaa914a5a99080a1b62
yujianson@163.com <yujianson@163.com>
2016-08-31 23:22:21 +0800

    first commit

---
+++ src/main/java/org/seckill/exception/SeckillCloseException.java
@@ -0,0 +1,17 @@
+package org.seckill.exception;
+
+
+/**
+ * 秒杀关闭异常
+ * Created by TS on 2016/5/20.
+ */
+public class SeckillCloseException extends SeckillException{
+
+    public SeckillCloseException(String message) {
+        super(message);
+    }
+
+    public SeckillCloseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
